7fb99d365532907d920b101cde7fc63371d01310
Freddie Mar A. Manegos <freddiemar.manegos@g.msuiit.edu.ph>
2020-07-21 08:49:57 +0800

    Setup Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { DefaultModule } from './layouts/default/default.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DefaultModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
